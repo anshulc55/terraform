c3b3d6ba69cd8bdd41d50c4a96c40805d09f1e08
Anshul Chauhan <anshulc55@gmail.com>
2020-10-07 21:27:43 +0530

    multi Instance

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,5 +1,10 @@
 
 resource "aws_instance" "MyFirstInstnace" {
+  count         = 3
   ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
+
+  tags{
+    Name = "threedemoinstance"
+  }
 }
