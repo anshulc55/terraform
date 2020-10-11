1707cca6a399c7a378b91fddf1d52eb3d4a37744
Anshul Chauhan <anshulc55@gmail.com>
2020-10-11 22:37:39 +0530

    Provision Software

---
+++ Terraform Concepts Building Blocks/createInstance.tf
@@ -0,0 +1,35 @@
+
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+
+  tags = {
+    Name = "custom_instance"
+  }
+
+  provisioner "file" {
+      source = "installNginx.sh"
+      destination = "/tmp/installNginx.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/installNginx.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/installNginx.sh",
+    ]
+  }
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+}
---
+++ Terraform Concepts Building Blocks/variables.tf
@@ -0,0 +1,37 @@
+variable "AWS_ACCESS_KEY" {
+    type = string
+    default = "AKIASMSIZOF42P2VUDSZ"
+}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+default = "us-east-2"
+}
+
+variable "Security_Group"{
+    type = list
+    default = ["sg-24076", "sg-90890", "sg-456789"]
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-0f40c8f97004632f9"
+        us-east-2 = "ami-05692172625678b4e"
+        us-west-2 = "ami-0352d5a37fb4f603f"
+        us-west-1 = "ami-0f40c8f97004632f9"
+    }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
