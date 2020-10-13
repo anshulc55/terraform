1593cc6e66b04b999c6d6be28258b9043087a088
Anshul Chauhan <anshulc55@gmail.com>
2020-10-13 21:51:57 +0530

    DataSource

--- Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
@@ -1,7 +1,11 @@
 
+data "aws_availability_zones" "avilable" {}
+
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.avilable.names[1]
 
   tags = {
     Name = "custom_instance"
