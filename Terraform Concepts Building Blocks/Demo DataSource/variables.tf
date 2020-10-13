96725e03023ea4b701f077eaed81418c03fc9b01
Anshul Chauhan <anshulc55@gmail.com>
2020-10-13 22:17:45 +0530

    data

--- Terraform Concepts Building Blocks/Demo DataSource/variables.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/variables.tf
@@ -17,8 +17,6 @@ variable "Security_Group"{
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
     }
