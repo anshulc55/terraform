6b05949ddca9afad8579b0c40f86866e06082b19
Anshul Chauhan <anshulc55@gmail.com>
2020-10-14 19:50:17 +0530

    Data Source SG

---
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -0,0 +1,20 @@
+data "aws_ip_ranges" "us_east_ip_range" {
+    regions = ["us-east-1","us-east-2"]
+    services = ["ec2"]
+}
+
+resource "aws_security_group" "sg-custom_us_east" {
+    name = "sg-custom_us_east"
+
+    ingress = {
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
+    }
+
+    tags = {
+        CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
+        SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
+    }
+}
