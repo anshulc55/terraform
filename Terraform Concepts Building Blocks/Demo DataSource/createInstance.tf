13b145351658e767e89e2fe3bda3de952444a5ae
Anshul Chauhan <anshulc55@gmail.com>
2020-10-15 20:16:48 +0530

    Output

--- Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
@@ -29,11 +29,8 @@ resource "aws_instance" "MyFirstInstnace" {
   tags = {
     Name = "custom_instance"
   }
+}
 
-  output "public_ip" {
-    value = aws_instance.MyFirstInstnace.public_ip 
-  }
-
-
-
+output "public_ip" {
+  value = aws_instance.MyFirstInstnace.public_ip 
 }
