dd8ccfbe3e0c19e711043358b13d037fd51a4fa2
Anshul Chauhan <anshulc55@gmail.com>
2020-10-15 20:13:58 +0530

    output Attributes

--- Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
@@ -22,8 +22,18 @@ resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.avilable.names[1]
 
+  provisioner "local-exec" {
+    command = "echo aws_instance.MyFirstInstnace.private_ip >> my_private_ips.txt"
+  }
+
   tags = {
     Name = "custom_instance"
   }
+
+  output "public_ip" {
+    value = aws_instance.MyFirstInstnace.public_ip 
+  }
+
+
 
 }
