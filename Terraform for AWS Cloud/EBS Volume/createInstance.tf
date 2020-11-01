08de93cfcb93cba887f4a27102b887eb6918d1d6
Anshul Chauhan <anshulc55@gmail.com>
2020-11-01 15:35:31 +0530

    EBS Instance

--- Terraform for AWS Cloud/EBS Volume/createInstance.tf
+++ Terraform for AWS Cloud/EBS Volume/createInstance.tf
@@ -8,6 +8,7 @@ resource "aws_key_pair" "levelup_key" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
   key_name      = aws_key_pair.levelup_key.key_name
 
   tags = {
