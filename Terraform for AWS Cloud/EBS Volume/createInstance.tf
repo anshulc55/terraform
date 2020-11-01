81010118e3feedcd3d1474a9117d46de2fd381b0
Anshul Chauhan <anshulc55@gmail.com>
2020-11-01 15:28:44 +0530

    EBS Attachment

---
+++ Terraform for AWS Cloud/EBS Volume/createInstance.tf
@@ -0,0 +1,34 @@
+
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+#Create AWS Instance
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+
+  tags = {
+    Name = "custom_instance"
+  }
+}
+
+#EBS resource Creation
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "us-east-2a"
+  size              = 50
+  type              = "gp2"
+
+  tags = {
+    Name = "Secondary Volume Disk"
+  }
+}
+
+#Atatch EBS volume with AWS Instance
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id   = aws_ebs_volume.ebs-volume-1.id
+  instance_id = aws_instance.MyFirstInstnace.id
+}
