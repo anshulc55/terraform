ad7e14fdee4ebdcd40dde5be39dd7a36e93738f5
Anshul Chauhan <anshulc55@gmail.com>
2020-11-01 19:50:54 +0530

    User Data Script

---
+++ Terraform for AWS Cloud/UserData/createInstance.tf
@@ -0,0 +1,26 @@
+
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+#Create AWS Instance
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name      = aws_key_pair.levelup_key.key_name
+
+  user_data = <<EOF
+		#! /bin/bash
+    sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed Machine via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
+
+  tags = {
+    Name = "custom_instance"
+  }
+}
