5d9c3537dc93d22da9d22aca7ecfca1f94db53c1
Anshul Chauhan <anshulc55@gmail.com>
2020-11-07 12:14:05 +0530

    AWS IAM

--- Terraform for AWS Cloud II/AWS IAM/s3bucket.tf
+++
@@ -1,9 +0,0 @@
-#Configuration for S3 Bucket
-resource "aws_s3_bucket" "levelup-bucket" {
-  bucket = "levelup-bucket-141"
-  acl    = "private"
-
-  tags = {
-    Name = "levelup-bucket-141"
-  }
-}
