d501c3b2029bd71c9ad66c96b9a6d1c6586fa5af
Anshul Chauhan <anshulc55@gmail.com>
2020-11-08 12:23:10 +0530

    Autoscaling EC2

--- Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
+++ Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
@@ -6,6 +6,12 @@ resource "aws_launch_configuration" "levelup-launchconfig" {
   key_name        = aws_key_pair.levelup_key.key_name
 }
 
+#Generate Key
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
 #Autoscaling Group
 resource "aws_autoscaling_group" "levelup-autoscaling" {
   name                      = "levelup-autoscaling"
