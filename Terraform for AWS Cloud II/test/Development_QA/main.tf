012e9fb3ed3d7dd6bda28295ebd23f6f2016aff9
Anshul Chauhan <anshulc55@gmail.com>
2020-11-26 20:43:59 +0530

    local path

--- Terraform for AWS Cloud II/test/Development_QA/main.tf
+++ Terraform for AWS Cloud II/test/Development_QA/main.tf
@@ -1,12 +1,13 @@
 module "dev-qa-vpc"{
-    source = "../../test"
+    source                          = "../../test"
 
-    vpcname = "dev02=qa-vpc"
-    cidr = "10.0.1.0/24"
-    enable_dns_support = "true"
-    enable_classiclink = "false"
-    enable_classiclink_dns_support = "false"
-    enable_ipv6 = "false"
-    vpcenvironment = "Development-QA-Engineering"
+    vpcname                         = "dev02=qa-vpc"
+    cidr                            = "10.0.1.0/24"
+    enable_dns_support              = "true"
+    enable_classiclink              = "false"
+    enable_classiclink_dns_support  = "false"
+    enable_ipv6                     = "false"
+    vpcenvironment                  = "Development-QA-Engineering"
+    AWS_REGION                      = "us-east-1"
 
 }
