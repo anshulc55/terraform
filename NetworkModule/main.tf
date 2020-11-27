9de1410fed6474d1d4d7531be9c96b3532883f81
Anshul Chauhan <anshulc55@gmail.com>
2020-11-27 22:55:33 +0530

    AWS Module

--- NetworkModule/main.tf
+++ NetworkModule/main.tf
@@ -18,8 +18,8 @@ resource "aws_key_pair" "levelup_key" {
 resource "aws_instance" "levelup_instance" {
   ami                       = var.instance_ami
   instance_type             = var.instance_type
-  subnet_id                 = module.network.public_subnet_id
-  vpc_security_group_ids    = ["${module.network.sg_22_id}"]
+  subnet_id                 = module.myvpc.public_subnet_id
+  vpc_security_group_ids    = ["${module.myvpc.sg_22_id}"]
   key_name                  = aws_key_pair.levelup_key.key_name
 
   tags = {
