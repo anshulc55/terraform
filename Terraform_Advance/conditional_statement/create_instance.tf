e2c7f979ab2fc23e73d2570f4d7f181c9e6e40a9
Anshul Chauhan <anshulc55@gmail.com>
2020-11-28 20:30:40 +0530

    Conditional

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -5,15 +5,15 @@ provider "aws" {
 module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-    name    = "my-cluster"
-    ami     = "ami-0f40c8f97004632f9"
-    instance_type          = "t2.micro"
-    subnet_id   = "subnet-e92f9cc8"
-    instance_count = var.environment == "Production" ? 2 : 1
+    name            = "my-cluster"
+    ami             = "ami-05692172625678b4e"
+    instance_type   = "t2.micro"
+    subnet_id       = "subnet-f30171bf"
+    instance_count  = var.environment == "Production" ? 2 : 1
 
 
     tags = {
-    Terraform   = "true"
-    Environment = var.environment
+    Terraform       = "true"
+    Environment     = var.environment
     }
 }
