d1ec96792e3b1d33f4dbd9eb6e313fd33e8d2f4a
Anshul Chauhan <anshulc55@gmail.com>
2020-12-02 22:01:43 +0530

    AMI Update

--- Terraform_Project_Structure/modules/instances/varaible.tf
+++ Terraform_Project_Structure/modules/instances/varaible.tf
@@ -23,8 +23,8 @@ variable "AMIS" {
     default = {
         us-east-1 = "ami-0f40c8f97004632f9"
         us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
+        us-west-2 = "ami-02c8896b265d8c480"
+        us-west-1 = "ami-055a0e87092ed2ba4"
     }
 }
 
