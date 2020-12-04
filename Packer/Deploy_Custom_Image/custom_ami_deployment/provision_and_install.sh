2f9a1c576607ce839728da608931b2b6c527cc9f
Anshul Chauhan <anshulc55@gmail.com>
2020-12-04 22:25:29 +0530

    Terraform Packer

--- Packer/Deploy_Custom_Image/custom_ami_deployment/provision_and_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ARTIFACT=`packer build -machine-readable install_custom_ami.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
-echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' >> varaiable.tf
-terraform init
-terraform apply
