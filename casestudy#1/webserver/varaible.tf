a69be92de93f8021ce09b473bcd7c4e65133696d
Anshul Chauhan <anshulc55@gmail.com>
2020-12-19 21:57:59 +0530

    Case Study 4

--- casestudy#1/webserver/varaible.tf
+++ casestudy#1/webserver/varaible.tf
@@ -13,3 +13,14 @@ variable "USER_DATA_FOR_WEBSERVER" {
     type = string
     default = "./demo_code/web.sh"
 }
+
+variable "AWS_REGION" {
+    type        = string
+    default     = "us-east-2"
+}
+
+variable "ENVIRONMENT" {
+  description = "AWS VPC Environment Name"
+  type        = string
+  default     = "Development"
+}
