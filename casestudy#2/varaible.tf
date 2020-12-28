598868ff064dabb91a15d6f94a94eb773558d2a3
Anshul Chauhan <anshulc55@gmail.com>
2020-12-28 21:39:56 +0530

    Terraform EKS

--- casestudy#2/varaible.tf
+++ casestudy#2/varaible.tf
@@ -8,11 +8,11 @@ variable "AMIS" {
     default = {
         us-east-1 = "ami-0f40c8f97004632f9"
         us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0107478b0b67378c8"
-        us-west-1 = "ami-0bf2eefa92a02a84f"
+        eu-west-2 = "ami-0107478b0b67378c8"
+        eu-west-1 = "ami-0bf2eefa92a02a84f"
     }
 }
 
 variable "AWS_REGION" {
-  default = "us-west-1"
+  default = "eu-west-1"
 }
