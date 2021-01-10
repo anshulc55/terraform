7d6774ff30432d4bcc551739adc593fa1d84cda5
Anshul Chauhan <anshulc55@gmail.com>
2021-01-10 22:03:55 +0530

    ELK Demo 1

--- casestudy#ELK/Demo-1/createinstance.tf
+++ casestudy#ELK/Demo-1/createinstance.tf
@@ -4,6 +4,51 @@ resource "aws_key_pair" "levelup_key" {
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+resource "aws_security_group" "allow_elk" {
+  name        = "allow_elk"
+  description = "All all elasticsearch traffic"
+
+  # elasticsearch port
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # logstash port
+  ingress {
+    from_port   = 5043
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # kibana ports
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 #Create AWS Instance
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
@@ -11,9 +56,19 @@ resource "aws_instance" "MyFirstInstnace" {
   availability_zone = "us-east-2a"
   key_name      = aws_key_pair.levelup_key.key_name
 
+  vpc_security_group_ids = [
+    aws_security_group.allow_elk.id,
+  ]
+
+  depends_on = [aws_security_group.allow_elk]
+  
   tags = {
     Name = "custom_instance"
   }
+}
+
+resource "aws_eip" "ip" {
+  instance = aws_instance.MyFirstInstnace.id
 }
 
 output "public_ip" {
