178d1449e00f5a45853c9424bae854ad47a763fa
Anshul Chauhan <anshulc55@gmail.com>
2021-01-12 21:05:30 +0530

    AWS ELK Demo

--- casestudy#ELK/Demo-2/installELK.sh
+++ casestudy#ELK/Demo-2/installELK.sh
@@ -4,6 +4,9 @@ sudo apt update
 sudo apt-get upgrade -y
 sudo apt-get install default-jre -y
 
+sudo apt-get install fish -y
+fish
+
 # install elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
@@ -11,7 +14,7 @@ echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" | sudo 
 
 sudo apt-get update
 sudo apt-get install elasticsearch -y
-
+sleep 10
 sudo mv /tmp/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
 sudo service elasticsearch start
@@ -19,17 +22,21 @@ sudo curl http://localhost:9200
 
 # install logstash
 sudo apt-get install logstash
+sleep 10
 
 # install kibana
 sudo apt-get install kibana
+sleep 10
 
 sudo mv /tmp/kibana.yml /etc/kibana/kibana.yml
 sudo service kibana start
 
 # install filebeats
 sudo apt-get install metricbeat
+sleep 10
 sudo service metricbeat start
 
 # Start LogStash
 sudo mv /tmp/apache-01.conf /etc/logstash/conf.d/apache-01.conf
+sleep 10
 sudo service logstash start
