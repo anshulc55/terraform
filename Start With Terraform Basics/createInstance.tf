4535adf0f03f0410123a595b3e3f9562c9b018e0
Anshul Chauhan <anshulc55@gmail.com>
2021-08-24 19:36:49 +0530

    Terraform

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
-  ami           = "ami-05692172625678b4e"
+  ami           = "ami-0e9ef0822841b8bb1"
   instance_type = "t2.micro"
 
   tags = {
