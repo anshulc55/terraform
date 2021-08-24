06269a60d806793164ec027b3f3988e5125bf1bd
Anshul Chauhan <anshulc55@gmail.com>
2021-08-24 19:52:57 +0530

    Terraform AMI

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -4,6 +4,9 @@
 provider "aws" {
     region = "us-east-1"
     version = "3.53.0"
+    access_key = "AKIASMSIZOF4QD2NTPB6"
+    secret_key = "SECRET_KEY_HERE"
+    region     = "us-east-2
 }
 
 # Configure the Microsoft Azure Provider
