40fabe4a4a68da260e966cd346881f953e71d0a9
Anshul Chauhan <anshul.chauhan@ukg.com>
2021-08-27 18:06:53 +0530

    GCP

---
+++ GCP Networking HandsOn Using Terraform/network.tf
@@ -0,0 +1,35 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+
+    project_id   = "<PROJECT ID>"
+    network_name = "my-vpc-netork"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-east1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-east1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This is Subnet Two."
+        }
+    ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
+    }
+}
