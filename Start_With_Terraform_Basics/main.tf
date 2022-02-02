59d07e61707ba497d3bcc6046d6c0b42dce07177
Andre Chagas <andre.chagas@oraex.com>
2022-02-02 15:26:48 -0300

    removed main.tf for testing

--- Start_With_Terraform_Basics/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "MyFirstInstnace" {
-  count         = 5
-  ami           = "ami-0a8b4cd432b1c3063"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "demoinstance-${count.index}"
-  }
-}
